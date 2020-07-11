f7d7b2903fd3022319e12938ad367aa910b97e49
Mapachana <anabuenrua99@gmail.com>
2020-07-11 14:34:49 +0200

    El robot ya salta y actualizadas animaciones para que no den toc

--- core/robot.gd
+++ core/robot.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
-const speed = 10.0
+const g = 20.0
+const speed = 50.0
+const jump_speed = 40.0
 
 var vel = Vector2(0.0, 0.0)
+var on_air
 
 var anim = ""
 
@@ -20,30 +23,39 @@ func _ready():
 func _process(delta):
 	move_input(delta) #Make the movement
 	
-	move_and_slide(speed * vel)
+	move_and_slide(vel, Vector2(0,-1))
 
 #	pass
 
 #This function accounts for the movement of the character
 func move_input(delta):
+	on_air = not is_on_floor()
+	
 	#Direction where should I move
-	if Input.is_action_pressed("ui_up"):
-		set_anim("idle")
-		vel = Vector2(0.0, -1.0)
-	elif Input.is_action_pressed("ui_down"):
-		set_anim("idle")
-		vel = Vector2(0.0, 1.0)
+	if Input.is_action_pressed("ui_up") and not on_air:
+		set_anim("walk")
+		vel.y = -jump_speed
+		on_air = true
+		print("hola")
+	#elif Input.is_action_pressed("ui_down"):
+	#	set_anim("idle")
+	#	vel = Vector2(0.0, 1.0)
 	elif Input.is_action_pressed("ui_right"):
 		set_anim("walk")
 		$Sprite.flip_h = false
-		vel = Vector2(1.0, 0.0)
+		vel.x = speed
 	elif Input.is_action_pressed("ui_left"):
 		set_anim("walk")
 		$Sprite.flip_h = true
-		vel = Vector2(-1.0, 0.0)
+		vel.x = -speed
 	else:
 		set_anim("idle")
-		vel = Vector2(0.0, 0.0)
+		vel.x = 0.0
+	
+	if on_air:
+		vel.y += delta*g
+	else:
+		vel.y = 0
 
 func set_anim(new_anim):
 	if (new_anim != anim):
