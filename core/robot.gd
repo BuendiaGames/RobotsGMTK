b5fa3c354d92f92581fe55488c46b707d9686caa
VictorSeven <vbuendiar@gmail.com>
2020-07-11 16:46:06 +0200

    Added random controls for player

--- core/robot.gd
+++ core/robot.gd
@@ -10,14 +10,22 @@ var currobj = null #object which you are interacting with
 
 var anim = ""
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const tau = 30.0 #Average time to change, in seconds
+
+#To make all input changes, these are available actions
+const actions = ["right", "left", "up", "down", "accionA", "accionB", "accionC"]
+var default_inputs = {}
+var current_action = "idle"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	
+	store_default_inputs()
+	
+	set_process(true)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -37,22 +45,23 @@ func move_input(delta):
 		set_anim("walk")
 		vel.y = -jump_speed
 		on_air = true
-		print("hola")
-	#elif Input.is_action_pressed("ui_down"):
-	#	set_anim("idle")
-	#	vel = Vector2(0.0, 1.0)
+		current_action = "ui_up"
 	elif Input.is_action_pressed("ui_right"):
 		set_anim("walk")
 		$Sprite.flip_h = false
 		vel.x = speed
+		current_action = "ui_right"
 	elif Input.is_action_pressed("ui_left"):
 		set_anim("walk")
 		$Sprite.flip_h = true
 		vel.x = -speed
+		current_action = "ui_left"
 	else:
 		set_anim("idle")
+		current_action = "idle"
 		vel.x = 0.0
 	
+	#Go down
 	if on_air:
 		vel.y += delta*g
 	else:
@@ -61,15 +70,57 @@ func move_input(delta):
 	if Input.is_action_just_pressed("ui_accionA") and currobj != null:
 			currobj.act()
 
+
+
+#Changes current animation
 func set_anim(new_anim):
 	if (new_anim != anim):
 		$AnimationPlayer.play(new_anim)
 		anim = new_anim
+
+
+#Stores all the InputEvents in a dictionary so all of them are available
+func store_default_inputs():
+	
+	
+	for action in actions:
+		action = "ui_" + action
+		default_inputs[action] = InputMap.get_action_list(action)[0]
+
+
+#Change all keys 
+func shuffle_input_map():
+	
+	var nact = len(actions)
+	var indices = range(nact)
+	
+	indices.shuffle()
+	
+	var action1 #Actions in order
+	var action2 #Shuffled stuff
+	
+	for j in range(nact):
+		action1 = "ui_" + actions[j]
+		action2 = "ui_" + actions[indices[j]]
+		InputMap.action_erase_events(action1)
+		InputMap.action_add_event(action1, default_inputs[action2])
 	
 
+#Shuffle input map and reinit the counter
+func _on_timeout():
+	$timer.wait_time = 30.0 + 10.0 * (2*randf()-1)
+	release_key()
+	shuffle_input_map() 
 
-
-
+#Reases the key that is actually pressed to avoid
+#confounding the input when the shuffle happens
+func release_key():
+	if current_action != "idle":
+		var ev = InputEventAction.new()
+		ev.action = current_action
+		ev.pressed = false
+		Input.parse_input_event(ev)
+	
 # update the current object
 func _on_Area2D_area_entered(area):
 	if area.name == "monitor":
@@ -80,3 +131,4 @@ func _on_Area2D_area_entered(area):
 func _on_Area2D_area_exited(area):
 	currobj = null
 	pass # Replace with function body.
+
