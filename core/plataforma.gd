b7ce4e5be01f38088f34e69780a90270ffda8142
Mapachana <anabuenrua99@gmail.com>
2020-07-11 17:51:21 +0200

    añadida plataforma, lucecitas y el comienzo de un robotcito

---
+++ core/plataforma.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+
+const speed = 30.0
+
+var vel = Vector2(0.0, 0.0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
